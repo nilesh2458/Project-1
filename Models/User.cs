8e5b34bb2af11729637de5a655502a033f268368
nilesh2458 <patharnileshbhai4@gmail.com>
2025-04-09 10:40:07 +0530

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementAPI.Models
+{
+    // Simple User model with basic validation
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+    }
+}
